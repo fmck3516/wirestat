a261aedfea0de8a35d3c1e68f745ac14be3a5a0e
Florian McKee <84742327+fmck3516@users.noreply.github.com>
2024-09-14 06:14:00 -0500

    more documentation

--- firmware/wirestat/wirestat.ino
+++ firmware/wirestat/wirestat.ino
@@ -1,3 +1,19 @@
+/*
+  Copyright 2024 the original author or authors.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
 #include <LiquidCrystal_I2C.h>
@@ -5,7 +21,7 @@
 const uint16_t PIN_IR_TRANSMITTER = 5;
 const uint16_t PIN_SDA = 21;
 const uint16_t PIN_SCL = 22;
-const uint16_t PIN_THERMOSTAT[] = { 
+const uint16_t PIN_THERMOSTAT[] = {
   16 /* G1 */, 
   17 /* G2 */, 
   18 /* G3 */, 
@@ -45,27 +61,27 @@ const int LCD_COLUMNS = 20;
 const int LCD_ROWS = 4;
 const int LCD_ADDRESS = 0x27;
 
-//
-// The number of calls to store. Must be at least 2 to store
-// - the candidate call and
-// - the active call
-//
+/*
+  The number of calls to store. Must be at least 2 to store
+  - the candidate call and
+  - the active call
+*/
 const int CALL_HISTORY = 5;
 
-//
-// History of the last ${CALL_HISTORY} calls:
-//
-// call[0]: the candidate call (that might be promoted to active or replaced with the next candidate call)
-// call[1]: the active call
-// call[2]: the previous call
-// call[3]: the pre-previous call
-// ...
-//
+/*
+  History of the last ${CALL_HISTORY} calls:
+
+  call[0]: the candidate call (that might be promoted to active or replaced with the next candidate call)
+  call[1]: the active call
+  call[2]: the previous call
+  call[3]: the pre-previous call
+  ...
+*/
 int call[CALL_HISTORY];
 
-//
-// The timestamps when the last ${CALL_HISTORY} calls started. 
-//
+/*
+  The timestamps when the calls started. 
+*/
 int callStart[CALL_HISTORY];
 
 const int CALL_IDX_CAND = 0;
@@ -214,39 +230,39 @@ bool checkThermostatPin(int pinIndex) {
   if (active) {
     PIN_LAST_ACTIVE[pinIndex] = millis();
   }
-  //
-  // If there was at least one call within the last ${DEBOUNCE_INTERVAL_MS} milliseconds, consider the pin to be called.
-  //
-  // The AC waveform that is fed into the optoinsulator results in intermittent false negatives when treating
-  // the output as digital signal. The time based heuristic eliminates the need for a capacitor to smoothen the
-  // output signal.
-  //
+  /*
+    If there was at least one call within the last ${DEBOUNCE_INTERVAL_MS} milliseconds, consider the pin to be called.
+  
+    The AC waveform that is fed into the optoinsulator results in intermittent false negatives when treating
+    the output as digital signal. The time based heuristic eliminates the need for a capacitor to smoothen the
+    output signal.
+  */
   return (millis() - PIN_LAST_ACTIVE[pinIndex]) < DEBOUNCE_INTERVAL_MS;
 }
 
 void processCallFromThermostat(int callFromThermostat) {
-  //
-  // A newly detected call from the thermostat is first treated as a "candidate call".
-  //
-  // Let's consider an example to understand the purpose of candidate calls:
-  //
-  // t=  0: thermostat calls for off
-  // t=100: thermostat calls for COOL Stage 2 (Y1 + Y2)
-  //
-  // The AC waveform that is fed into the optoinsulators and the timing of code executed can result in the following
-  // calls from the thermostat to be observed:
-  //
-  // t=  1: wirestat detects call OFF          (all pins inactive)
-  // t=101: wirestat detects call COOL Stage 1 (Y1 active / Y2 inactive)
-  // t=102: wirestat detects call COOL Stage 2 (Y1 active / Y2 active)
-  //
-  // The thermostat never calls for COOL Stage 1. wirestat detects this call because there is a moment in time where it
-  // reads Y1 as active and Y2 as inactive. Shortly afterwards, Y2 reads as active and the intended call (COOL Stage 2)
-  // is detected.
-  //
-  // Treating newly detected calls as candidates allows wirestat to detect and ignore calls that are only active for 
-  // a very short time.
-  //
+  /*
+    A newly detected call from the thermostat is first treated as a "candidate call".
+
+    Let's consider an example to understand the purpose of candidate calls:
+
+    t=  0: thermostat calls for off
+    t=100: thermostat calls for COOL Stage 2 (Y1 + Y2)
+
+    The AC waveform that is fed into the optoinsulators and the timing of code executed can result in the following
+    calls from the thermostat to be observed:
+
+    t=  1: wirestat detects call OFF          (all pins inactive)
+    t=101: wirestat detects call COOL Stage 1 (Y1 active / Y2 inactive)
+    t=102: wirestat detects call COOL Stage 2 (Y1 active / Y2 active)
+
+    The thermostat never calls for COOL Stage 1. However, wirestat detects this call because there is a moment in time 
+    where it reads Y1 as active and Y2 as inactive. Shortly afterwards, Y2 reads as active and the intended call 
+    (COOL Stage 2) is detected.
+
+    Treating newly detected calls as candidates allows wirestat to filter out "phantom calls" like COOL Stage 1 in the 
+    above example.
+  */
   if (call[CALL_IDX_CAND] != callFromThermostat) {
       
     call[CALL_IDX_CAND] = callFromThermostat;
@@ -254,31 +270,31 @@ void processCallFromThermostat(int callFromThermostat) {
     
   } else {    
 
-    //
-    // If the candidate call is detected for a time period longer than ${DEBOUNCE_INTERVAL_MS}, it is promoted to be the
-    // next active call.
-    //
+    /*
+      If the candidate call is detected for a time period longer than ${DEBOUNCE_INTERVAL_MS}, it is promoted to be the
+      next active call.
+    */
     bool promoteCandidateToActive = call[CALL_IDX_CAND] != call[CALL_IDX_ACTIVE] &&
       (millis() - callStart[CALL_IDX_CAND] > DEBOUNCE_INTERVAL_MS);
     if (promoteCandidateToActive) {
       
-      //
-      // Shift the history to make room for the new active call.
-      //
+      /*
+        Shift the history to make room for the new active call.
+      */
       for (int i = CALL_HISTORY - 1; i >= 2; i--) {
         call[i] = call[i - 1];
         callStart[i] = callStart[i - 1];
       }
       
-      //
-      // Promote the candidate to active.
-      //
+      /*
+        Promote the candidate to active.
+      */
       call[CALL_IDX_ACTIVE] = call[CALL_IDX_CAND];
       callStart[CALL_IDX_ACTIVE] = millis();
       
-      //
-      // Send new active call to the indoor unit.
-      //
+      /*
+        Send new active call to the indoor unit.
+      */
       sendMessage();
     }    
   }
